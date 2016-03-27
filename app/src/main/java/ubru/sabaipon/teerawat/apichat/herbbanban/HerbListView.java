22e8e5272a0176de9bfbfe4b376cf41c199979af
muy <muy721@hotmail.com>
2016-03-27 15:37:10 +0700

    ได้ทำการ Create Map เรียบร้อยแล้ว ผ่าน

--- app/src/main/java/ubru/sabaipon/teerawat/apichat/herbbanban/HerbListView.java
+++ app/src/main/java/ubru/sabaipon/teerawat/apichat/herbbanban/HerbListView.java
@@ -1,10 +1,13 @@
 package ubru.sabaipon.teerawat.apichat.herbbanban;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.google.android.gms.appindexing.Action;
@@ -39,55 +42,25 @@ public class HerbListView extends AppCompatActivity {
             imageStrings[i] = cursor.getString(2);
 
             cursor.moveToNext();
+       }   //for
+        cursor.close();
 
-            HerbAdapter herbAdapter = new HerbAdapter(this, imageStrings, nameStrings);
-            ListView herbListView = (ListView) findViewById(R.id.listView);
-            herbListView.setAdapter(herbAdapter);
+        HerbAdapter herbAdapter = new HerbAdapter(this, imageStrings, nameStrings);
+        ListView herbListView = (ListView) findViewById(R.id.listView);
+        herbListView.setAdapter(herbAdapter);
 
-        }   //for
+        herbListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+                Intent intent = new Intent(HerbListView.this, DetailActivity.class);
+                startActivity(intent);
+
+            }   //onItem
+        });
+
+
     }   //Main Method
 
-    @Override
-    public void onStart() {
-        super.onStart();
 
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "HerbListView Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://ubru.sabaipon.teerawat.apichat.herbbanban/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "HerbListView Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://ubru.sabaipon.teerawat.apichat.herbbanban/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
-    }
 }   //Main Class
