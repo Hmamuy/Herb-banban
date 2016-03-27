5875690e2588a81acc4b0262983a1c0f4f3c3e8e
muy <muy721@hotmail.com>
2016-03-27 17:13:47 +0700

    ได้ทำการ show Finis บ้าน master เรียบร้อยแล้ว ผ่าน

--- app/src/main/java/ubru/sabaipon/teerawat/apichat/herbbanban/ReadAllHerbActivity.java
+++ app/src/main/java/ubru/sabaipon/teerawat/apichat/herbbanban/ReadAllHerbActivity.java
@@ -4,13 +4,20 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class ReadAllHerbActivity extends AppCompatActivity {
+
+    //Explicit
+    private String[] myResultStrings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_read_all_herb);
+
+        myResultStrings = getIntent().getStringArrayExtra("Data");
+
     }   //Main Method
 
     public void ClickReadAll(View view) {
@@ -21,9 +28,20 @@ public class ReadAllHerbActivity extends AppCompatActivity {
 
     public void ClickUpdate(View view) {
 
+        startActivity(new Intent(this, UpdateHerb.class));
+
     }
 
     public void ClickApprove (View view) {
+
+        if (myResultStrings[3].equals("0")) {
+            startActivity(new Intent(this, ApproveHerb.class));
+
+        } else {
+            Toast.makeText(this, "คุณไม่มีสิทธิเข้าถึงข้อมูล ค่ะ", Toast.LENGTH_SHORT).show();
+
+        }
+
 
     }
 }   //Main Class
